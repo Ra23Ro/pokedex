37d1e0dbcc18c5e82e78328524e943a0712623bb
Ra23Ro <roberto_cruzmendoza@hotmail.com>
2020-07-25 15:44:57 -0500

    gitgit config

---
+++ src/components/Pokemon.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="container">
+    <img src="pokemonImg.png" style="width: 400px; height:150px;">
+    <PokemonBuscar
+      :apiUrl="apiUrl" 
+      @setPokemonUrl="setPokemonUrl" />
+    <PokemonLista
+      :imageUrl="imageUrl" 
+      :apiUrl="apiUrl"
+      @setPokemonUrl="setPokemonUrl" />
+    <PokemonDetalle
+      v-if="showDetail"
+      :pokemonUrl="pokemonUrl"
+      :imageUrl="imageUrl"
+      @closeDetail="closeDetail" />
+  </div>
+</template>
+
+<script>
+  import PokemonBuscar from './PokemonBuscar.vue';
+  import PokemonLista from './PokemonLista.vue';
+  import PokemonDetalle from './PokemonDetalle.vue';
+
+  export default {
+    data: () => {
+      return {
+        imageUrl: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/',
+        apiUrl: 'https://pokeapi.co/api/v2/pokemon/',
+        pokemonUrl: '',
+        showDetail: false
+      }
+    },
+    components: {
+      PokemonBuscar,
+      PokemonLista,
+      PokemonDetalle
+    },
+    methods: {
+      setPokemonUrl(url) {
+        this.pokemonUrl = url;
+        this.showDetail = true;
+      },
+      closeDetail() {
+        this.pokemonUrl = '';
+        this.showDetail = false;
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  @import url('https://fonts.googleapis.com/css?family=Acme');
+
+  .container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    padding: 10px;
+    width: calc(100% - 20px);
+    min-height: calc(100vh - 20px);
+    font-family: 'Acme', arial;
+    font-size: 1rem;
+    font-weight: normal;
+  }
+
+  h1 { color: #efefef; }
+</style>
